cca8fd1ebb43e16eb604a327c55ad86c95487488
brigitte <mbpadilla4@misena.edu.co>
2023-11-26 12:51:03 -0500

    Actualizando la página de Contacto

---
+++ app/src/main/java/com/sena/timco/TresActivity.java
@@ -0,0 +1,14 @@
+package com.sena.timco;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TresActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tres);
+    }
+}
